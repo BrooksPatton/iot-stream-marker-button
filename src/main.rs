a15912d5f2c8c227f047f05ba858cd134be3dd65
Brooks Patton <brooks@brookzerker>
2019-01-19 15:35:39 -0700

    trying 8

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use gpio::{GpioIn, GpioOut};
 use std::{thread, time};
 
 fn main() {
-    let mut gpio14 = gpio::sysfs::SysFsGpioInput::open(14).unwrap();
+    let mut gpio14 = gpio::sysfs::SysFsGpioInput::open(8).unwrap();
 
     loop {
         println!("GPIO14: {:?}", gpio14.read_value().unwrap());
